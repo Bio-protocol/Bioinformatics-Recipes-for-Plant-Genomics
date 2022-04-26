59aff02de8162ac8a45939100bde7d366eaadb74
Zhiqiang-hao <70687768+Zhiqiang-hao@users.noreply.github.com>
2022-04-26 21:23:04 +0800

    Update 3_run_cutadapt.sh

--- Workflow/3_run_cutadapt.sh
+++ Workflow/3_run_cutadapt.sh
@@ -23,3 +23,5 @@ mycanonicalpath=$(canonicalPath "$../")
 
 
 cutadapt -a AGATCGGAAGAGC -A AGATCGGAAGAGC -q 30 -m 20 –trim-n -O 10 -o Output/cutadapt/SRR2061397_1trimmed.fastq -p Output/cutadapt/SRR2061397_2trimmed.fasq  Input/SRR2061397_1.fastq Input/SRR2061397_2.fastq
+
+#Parameter description: -a for sequence of an adapter ligated to the 3’ end, -A 3’ adapter to be removed from second read in a pair, -q trim low-quality bases from 5’ and 3’ ends of each read. -m 20 for discard trimmed reads that are shorter than 20. -O MINLENGTH if the overlap between the read and the adapter is shorter than MINLENGTH, the read is not modified, reduces the number of bases trimmed due to random adapter match. -o output file -p paired-output.
